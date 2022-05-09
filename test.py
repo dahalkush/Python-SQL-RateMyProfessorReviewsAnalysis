1a1ee83949579afc1724b28485a7d0bc24b078f2
dahalkush <89929970+dahalkush@users.noreply.github.com>
2022-05-09 10:51:57 -0400

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-data = [[{'label': 'sadness', 'score': 0.0035145876463502645}, {'label': 'joy', 'score': 0.003847507294267416},
-         {'label': 'love', 'score': 0.0007544109830632806}, {'label': 'anger', 'score': 0.9888659119606018},
-         {'label': 'fear', 'score': 0.0025562464725226164}, {'label': 'surprise', 'score': 0.00046126183588057756}]]
-
-
-print(data[0])
