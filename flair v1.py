c9a89769cbfa6e37e9f688d6f434be1125a5bd79
dahalkush <89929970+dahalkush@users.noreply.github.com>
2022-05-09 10:52:36 -0400

    Delete flair v1.py

--- flair v1.py
+++
@@ -1,9 +0,0 @@
-from flair.models import TextClassifier
-from flair.data import Sentence
-
-classifier = TextClassifier.load('en-sentiment')
-sentence = Sentence('The food was great!')
-classifier.predict(sentence)
-
-# print sentence with predicted labels
-print('Sentence above is: ', sentence.labels)
